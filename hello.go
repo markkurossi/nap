a722a9e76af3bdc6f02dc5712ab9aa3dbb538af2
Markku Rossi <mtr@iki.fi>
2024-08-06 14:01:06 +0200

    Updated hello screen.

--- hello.go
+++ hello.go
@@ -13,5 +13,20 @@ import (
 // Hello implements handler for healthcheck calls.
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte("<body><h1>Hello, world!</h1>\n"))
+	w.Write([]byte(`<!DOCTYPE html>
+<head>
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 16 16'><text x='0' y='14'>🚫</text></svg>" />
+  <title>NAP</title>
+  <style>
+    body {
+        font-family: monospace;
+    }
+  </style>
+</head>
+<html lang="en">
+  <body>
+    <h1>&#x1f6ab; No Advertising Please &#x1f6ab;</h1>
+  </body>
+</html>
+`))
 }
