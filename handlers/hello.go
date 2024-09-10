d1c39164130d81768477ed67314d0c610f56cad2
Markku Rossi <mtr@iki.fi>
2024-09-10 15:49:32 +0200

    Generate valid HTML.

--- handlers/hello.go
+++ handlers/hello.go
@@ -14,16 +14,16 @@ import (
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(`<!DOCTYPE html>
-<head>
-  <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 16 16'><text x='0' y='14'>🚫</text></svg>" />
-  <title>NAP</title>
-  <style>
-    body {
-        font-family: monospace;
-    }
-  </style>
-</head>
 <html lang="en">
+  <head>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 16 16'><text x='0' y='14'>🚫</text></svg>" >
+    <title>NAP</title>
+    <style>
+        body {
+            font-family: monospace;
+        }
+    </style>
+  </head>
   <body>
     <h1>&#x1f6ab; No Advertising Please &#x1f6ab;</h1>
   </body>
